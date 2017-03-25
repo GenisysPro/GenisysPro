b3570f6117b5cc9da92f4e3bb56091aa7084fae4
SuperXingKong <1912003473@qq.com>
2017-03-25 09:19:53 +0800

    OOPS

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2196,7 +2196,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 					break;
 				}
 
-				if(!in_array($packet->protocol, ProtocolInfo::CURRENT_PROTOCOL)){
+				if(!in_array($packet->protocol, ProtocolInfo::ACCEPTED_PROTOCOLS)){
 					if($packet->protocol < ProtocolInfo::CURRENT_PROTOCOL){
 						$message = "disconnectionScreen.outdatedClient";
 
