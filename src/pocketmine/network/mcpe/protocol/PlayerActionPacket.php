5dc5f1b27f41504fa3a3283b54868051f0f371ed
SuperXingKong <1912003473@qq.com>
2017-04-11 22:17:22 +0800

    PlayerActionPacket for Genisys

--- src/pocketmine/network/mcpe/protocol/PlayerActionPacket.php
+++ src/pocketmine/network/mcpe/protocol/PlayerActionPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,7 +15,7 @@
  *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
- *
+ * 
  *
 */
 
@@ -27,6 +27,7 @@ namespace pocketmine\network\mcpe\protocol;
 use pocketmine\network\mcpe\NetworkSession;
 
 class PlayerActionPacket extends DataPacket{
+
 	const NETWORK_ID = ProtocolInfo::PLAYER_ACTION_PACKET;
 
 	const ACTION_START_BREAK = 0;
@@ -36,13 +37,14 @@ class PlayerActionPacket extends DataPacket{
 
 	const ACTION_RELEASE_ITEM = 5;
 	const ACTION_STOP_SLEEPING = 6;
-	const ACTION_RESPAWN = 7;
+	const ACTION_SPAWN_SAME_DIMENSION = 7;
 	const ACTION_JUMP = 8;
 	const ACTION_START_SPRINT = 9;
 	const ACTION_STOP_SPRINT = 10;
 	const ACTION_START_SNEAK = 11;
 	const ACTION_STOP_SNEAK = 12;
-	const ACTION_DIMENSION_CHANGE = 13; //TODO: correct these
+	const ACTION_SPAWN_OVERWORLD = 13;
+	const ACTION_SPAWN_NETHER = 14;
 
 	const ACTION_START_GLIDE = 15;
 	const ACTION_STOP_GLIDE = 16;
@@ -71,6 +73,13 @@ class PlayerActionPacket extends DataPacket{
 
 	public function handle(NetworkSession $session) : bool{
 		return $session->handlePlayerAction($this);
+	}
+
+	/**
+	 * @return PacketName|string
+     */
+	public function getName(){
+		return "PlayerActionPacket";
 	}
 
 }
