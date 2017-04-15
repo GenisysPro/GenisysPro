820a3c83b2856de16e8c6d3e5e90645ee1e544bd
SuperXingKong <1912003473@qq.com>
2017-04-15 13:28:16 +0800

    Improve Level (Part 2)

--- src/pocketmine/event/LevelTimings.php
+++ src/pocketmine/event/LevelTimings.php
@@ -25,6 +25,12 @@ use pocketmine\level\Level;
 
 class LevelTimings{
 
+    /** @var TimingsHandler */
+    public $setBlock;
+    /** @var TimingsHandler */
+    public $doBlockLightUpdates;
+    /** @var TimingsHandler */
+    public $doBlockSkyLightUpdates;
 	/** @var TimingsHandler */
 	public $mobSpawn;
 	/** @var TimingsHandler */
@@ -78,6 +84,10 @@ class LevelTimings{
 
 	public function __construct(Level $level){
 		$name = $level->getFolderName() . " - ";
+
+        $this->setBlock = new TimingsHandler("** " . $name . "setBlock");
+        $this->doBlockLightUpdates = new TimingsHandler("** " . $name . "doBlockLightUpdates");
+        $this->doBlockSkyLightUpdates = new TimingsHandler("** " . $name . "doBlockSkyLightUpdates");
 
 		$this->mobSpawn = new TimingsHandler("** " . $name . "mobSpawn");
 		$this->doChunkUnload = new TimingsHandler("** " . $name . "doChunkUnload");
