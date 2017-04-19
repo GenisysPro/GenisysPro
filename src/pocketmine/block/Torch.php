b110c69286f8e3bb2849b944e7831131c36f29ff
SuperXingKong <1912003473@qq.com>
2017-04-20 06:26:37 +0800

    Fixed #15 :D

--- src/pocketmine/block/Torch.php
+++ src/pocketmine/block/Torch.php
@@ -59,8 +59,8 @@ class Torch extends Flowable{
 			if($this->getSide($faces[$side])->isTransparent() === true and
 					!($side === 0 and ($below->getId() === self::FENCE or
 									$below->getId() === self::COBBLE_WALL or
-									$below->getId() == Block::INACTIVE_REDSTONE_LAMP or
-									$below->getId() == Block::ACTIVE_REDSTONE_LAMP)
+									$below->getId() == Block::REDSTONE_LAMP or
+									$below->getId() == Block::LIT_REDSTONE_LAMP)
 					)
 			){
 				$this->getLevel()->useBreakOn($this);
@@ -90,8 +90,8 @@ class Torch extends Flowable{
 		}elseif(
 				$below->isTransparent() === false or $below->getId() === self::FENCE or
 				$below->getId() === self::COBBLE_WALL or
-				$below->getId() == Block::INACTIVE_REDSTONE_LAMP or
-				$below->getId() == Block::ACTIVE_REDSTONE_LAMP
+				$below->getId() == Block::REDSTONE_LAMP or
+				$below->getId() == Block::LIT_REDSTONE_LAMP
 		){
 			$this->meta = 0;
 			$this->getLevel()->setBlock($block, $this, true, true);
