4e069525cffac50f8d3ed4767b4899f593a44a0c
MC-Nation <herojhjh@outlook.com>
2017-05-05 09:21:11 +0700

    ok

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -35,7 +35,7 @@ interface Info{
 	 */
 	const CURRENT_PROTOCOL = 108;
 	const ACCEPTED_PROTOCOLS = [105,106,107,108];	
-	const MINECRAFT_VERSION = ["v1.0.5","v1.0.6","v1.0.7,"v1.0.8"];
+	const MINECRAFT_VERSION = ["v1.0.5","v1.0.6","v1.0.7","v1.0.8"];
 	const MINECRAFT_VERSION_NETWORK = "1.0.8";
 
 	const LOGIN_PACKET = 0x01;
