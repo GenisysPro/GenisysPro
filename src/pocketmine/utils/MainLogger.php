e69d6f1e3f6df9d8799b07a89b744d07b9922baf
chadigos <3296764554@qq.com>
2017-05-06 02:52:33 +0800

    Update MainLogger.php

--- src/pocketmine/utils/MainLogger.php
+++ src/pocketmine/utils/MainLogger.php
@@ -219,7 +219,7 @@ class MainLogger extends \AttachableThreadedLogger{
 		}
 
     //$message = TextFormat::toANSI(TextFormat::AQUA . "[" . date("H:i:s", $now) . "] " . TextFormat::RESET . $color . "[" . $threadName . "/" . $prefix . "]:" . " " . $message . TextFormat::RESET);
-	$message = TextFormat::toANSI(TextFormat::AQUA . "[GenisysPro]->[" . date("H:i:s", $now) . "] " . TextFormat::RESET . $color . "[$prefix ]:" . " " . $message . TextFormat::RESET);
+	$message = TextFormat::toANSI(TextFormat::AQUA . "[GenisysPro]->[" . date("H:i:s", $now) . "] " . TextFormat::RESET . $color . "[$prefix]:" . " " . $message . TextFormat::RESET);
 	//$message = TextFormat::toANSI(TextFormat::AQUA . "[" . date("H:i:s") . "] ". TextFormat::RESET . $color ."<".$prefix . ">" . " " . $message . TextFormat::RESET);
 		$cleanMessage = TextFormat::clean($message);
 
