816d7d54e80dc56df64ffc2b5e9bc8439625d7ed
SuperXingKong <1912003473@qq.com>
2017-05-08 18:07:46 +0800

    1.0.8 has the same protocol,Fixed #28

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -33,8 +33,8 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 108;
-	const ACCEPTED_PROTOCOLS = [105,106,107,108];	
+	const CURRENT_PROTOCOL = 107;
+	const ACCEPTED_PROTOCOLS = [105,106,107];	
 	const MINECRAFT_VERSION = ["v1.0.5","v1.0.6","v1.0.7","v1.0.8"];
 	const MINECRAFT_VERSION_NETWORK = "1.0.8";
 
