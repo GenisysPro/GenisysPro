3a003f3407a7febae87570825b651b2317241c04
yuseung01 <yuseung2004@gmail.com>
2017-06-04 00:08:45 +0900

    Change MINECRAFT_VERSION_NETWORK to 1.1.0.55 (#68)
    Don't delete ACCEPTED_PROTOCOLS since Mojang only change the protocol number.

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -38,7 +38,7 @@ interface Info{
 	const CURRENT_PROTOCOL = 113;
 	const ACCEPTED_PROTOCOLS = [110,111,112,113];
 	const MINECRAFT_VERSION = ["v1.1.0.0","v1.1.0.8","v1.1.0.9","v1.1.0.55"];
-	const MINECRAFT_VERSION_NETWORK = "1.1.0.9";
+	const MINECRAFT_VERSION_NETWORK = "1.1.0.55";
 	
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
@@ -135,4 +135,3 @@ interface Info{
 	const PURCHASE_RECEIPT_PACKET = 0x5d;
 
 }
-
