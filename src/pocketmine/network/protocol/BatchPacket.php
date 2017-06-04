286eb1370f85d632474d2c29a7e71ec08272ad6b
SuperXingKong <1912003473@qq.com>
2017-06-04 18:38:14 +0800

    Fixed

--- src/pocketmine/network/protocol/BatchPacket.php
+++ src/pocketmine/network/protocol/BatchPacket.php
@@ -24,18 +24,18 @@ namespace pocketmine\network\protocol;
 #include <rules/DataPacket.h>
 
 
-class BatchPacket extends DataPacket {
+class BatchPacket extends DataPacket{
+	const NETWORK_ID = 0xfe;
 
-    const NETWORK_ID = Info::BATCH_PACKET;
+	public $payload;
 
-    public $payload;
+	public function decode(){
+		$this->payload = $this->get(true);
+	}
 
-    public function decode() {
-        $this->payload = $this->getString();
-    }
+	public function encode(){
+		$this->reset();
+		$this->put($this->payload);
+	}
 
-    public function encode() {
-        $this->reset();
-        $this->putString($this->payload);
-    }
 }
