28d9f0a590067f22043ab7fb24279350880397b3
SuperXingKong <1912003473@qq.com>
2017-06-16 19:59:04 +0800

    Fixed breaking, resolved #149
    Did this commit fix another block break issue #150 :?
    Plz test it.

--- src/pocketmine/network/protocol/PlayerActionPacket.php
+++ src/pocketmine/network/protocol/PlayerActionPacket.php
@@ -43,9 +43,10 @@ class PlayerActionPacket extends DataPacket{
 	const ACTION_STOP_SNEAK = 12;
 	const ACTION_SPAWN_OVERWORLD = 13;
 	const ACTION_SPAWN_NETHER = 14;
-
 	const ACTION_START_GLIDE = 15;
 	const ACTION_STOP_GLIDE = 16;
+
+	const ACTION_CONTINUE_BREAK = 18;
 
 	public $eid;
 	public $action;
