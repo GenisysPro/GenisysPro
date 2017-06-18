cc32f910bfb8e7be4d4aafd212541e9b6882b726
Realm <Kavindujayasinghe2000@Gmail.com>
2017-06-18 18:15:14 +0530

    New Mobs

---
+++ src/pocketmine/entity/Donkey.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ *
+ *  _____            _               _____           
+ * / ____|          (_)             |  __ \          
+ *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
+ *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
+ *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
+ * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
+ *                         __/ |                    
+ *                        |___/                     
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author GenisysPro
+ * @link https://github.com/GenisysPro/GenisysPro
+ *
+ *
+ */
+
+namespace pocketmine\entity;
+
+use pocketmine\item\Item as ItemItem;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
+
+class Donkey extends Animal{
+	const NETWORK_ID = 24;
+
+	public $width = 0.3;
+	public $length = 0.9;
+	public $height = 0;
+	
+	public $dropExp = [1, 3];
+	
+	public function getName(){
+		return "Donkey";
+	}
+	
+	public function initEntity(){
+		$this->setMaxHealth(20);
+		parent::initEntity();
+	}
+	
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = Donkey::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->yaw = $this->yaw;
+		$pk->pitch = $this->pitch;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
+
+		parent::spawnTo($player);
+	}
+	
+	public function getDrops(){
+		$drops = [
+			ItemItem::get(ItemItem::LEATHER, 0, mt_rand(1 , 2))
+		];
+
+		return $drops;
+	}
+}
---
+++ src/pocketmine/entity/WitherSkeleton.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ *
+ *  _____            _               _____           
+ * / ____|          (_)             |  __ \          
+ *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
+ *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
+ *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
+ * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
+ *                         __/ |                    
+ *                        |___/                     
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author GenisysPro
+ * @link https://github.com/GenisysPro/GenisysPro
+ *
+ *
+ */
+
+namespace pocketmine\entity;
+
+use pocketmine\item\Item as ItemItem;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
+
+class WitherSkeleton extends Monster{
+	const NETWORK_ID = 48;
+	
+	public $width = 0.3;
+	public $length = 0.9;
+	public $height = 0;
+	
+	public $dropExp = [5, 5];
+	
+	public function getName(){
+		return "Wither Skeleton";
+	}
+	
+	public function initEntity(){
+		$this->setMaxHealth(20);
+		parent::initEntity();
+	}
+	
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = WitherSkeleton::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->yaw = $this->yaw;
+		$pk->pitch = $this->pitch;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
+
+		parent::spawnTo($player);
+	}
+	
+	public function getDrops(){
+		$drops = [
+			ItemItem::get(ItemItem::COAL, 0, mt_rand(0 , 1))
+		];
+		$drops[] = ItemItem::get(ItemItem::BONE, 0, mt_rand(0 , 2));
+
+		return $drops;
+	}
+}
