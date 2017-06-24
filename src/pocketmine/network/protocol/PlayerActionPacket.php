2cb2dad649594ccfcf7722a4bbcfc57c48a02ad0
SuperKali <danychannel18@gmail.com>
2017-06-24 14:32:09 +0200

    Added build-denied action constant

--- src/pocketmine/network/protocol/PlayerActionPacket.php
+++ src/pocketmine/network/protocol/PlayerActionPacket.php
@@ -45,6 +45,8 @@ class PlayerActionPacket extends DataPacket{
 	const ACTION_SPAWN_NETHER = 14;
 	const ACTION_START_GLIDE = 15;
 	const ACTION_STOP_GLIDE = 16;
+	
+	const ACTION_BUILD_DENIED = 17;
 
 	const ACTION_CONTINUE_BREAK = 18;
 
