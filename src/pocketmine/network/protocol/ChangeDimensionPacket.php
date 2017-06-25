000bd7dff3347042ec4b266e1ed6e5e32cef59e7
SuperXingKong <1912003473@qq.com>
2017-06-25 17:54:19 +0800

    Find End constant of Dimension
    TODO: Add End

--- src/pocketmine/network/protocol/ChangeDimensionPacket.php
+++ src/pocketmine/network/protocol/ChangeDimensionPacket.php
@@ -2,22 +2,25 @@
 
 /*
  *
- *  _____   _____   __   _   _   _____  __    __  _____
- * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
- * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
- * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
- * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
- * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *  _____            _               _____           
+ * / ____|          (_)             |  __ \          
+ *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
+ *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
+ *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
+ * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
+ *                         __/ |                    
+ *                        |___/                     
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author iTX Technologies
- * @link https://itxtech.org
+ * @author GenisysPro
+ * @link https://github.com/GenisysPro/GenisysPro
  *
- */
+ *
+*/
 
 namespace pocketmine\network\protocol;
 
@@ -29,6 +32,7 @@ class ChangeDimensionPacket extends DataPacket{
 
 	const DIMENSION_NORMAL = 0;
 	const DIMENSION_NETHER = 1;
+	const DIMENSION_END = 2; //TODO: End
 
 	public $dimension;
 
@@ -46,13 +50,6 @@ class ChangeDimensionPacket extends DataPacket{
 		$this->putVarInt($this->dimension);
 		$this->putVector3f($this->x, $this->y, $this->z);
 		$this->putBool($this->unknown);
-	}
-
-	/**
-	 * @return PacketName|string
-	 */
-	public function getName(){
-		return "ChangeDimensionPacket";
 	}
 
 }
