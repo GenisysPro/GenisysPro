65bfc011e5559a0ef6e896029ddf6a0750211be4
CookieCode <etienne.crosnier.la@gmail.com>
2017-07-04 02:58:52 +0200

    Ban command fixed, Close #304
    Issue https://github.com/GenisysPro/GenisysPro/issues/304 Fixed.

--- src/pocketmine/command/defaults/BanCommand.php
+++ src/pocketmine/command/defaults/BanCommand.php
@@ -1,13 +1,6 @@
 <?php
 
 /*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
  *  _____            _               _____           
  * / ____|          (_)             |  __ \          
  *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
@@ -16,7 +9,6 @@
  * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
  *                         __/ |                    
  *                        |___/                     
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -35,14 +27,13 @@ use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\Player;
 
-
 class BanCommand extends VanillaCommand{
 
 	public function __construct($name){
 		parent::__construct(
 			$name,
 			"%pocketmine.command.ban.player.description",
-			"%pocketmine.command.ban.player.ban.usage"
+			"%commands.ban.usage"
 		);
 		$this->setPermission("pocketmine.command.ban.player");
 	}
@@ -59,21 +50,13 @@ class BanCommand extends VanillaCommand{
 		}
 
 		$name = array_shift($args);
-		if(isset($args[0]) and isset($args[1])){
-			$reason = implode(" ", $args);
-			if(is_numeric(end($args))){
-				$reason = str_replace(end($args), " ", $reason);
-				$until = new \DateTime('@' . (end($args) * 86400 + time()));
-				$sender->getServer()->getNameBans()->addBan($name, $reason, $until, $sender->getName());
-			}else{
-				$until = null;
-				$sender->getServer()->getNameBans()->addBan($name, $reason = implode(" ", $args), $until, $sender->getName());
-			}	
-		} else {
-			$sender->getServer()->getNameBans()->addBan($name);
-		}
+		$reason = implode(" ", $args);
 
-        $player = $sender->getServer()->getPlayerExact($name);
+		$sender->getServer()->getNameBans()->addBan($name, $reason, null, $sender->getName());
+
+		if(($player = $sender->getServer()->getPlayerExact($name)) instanceof Player){
+			$player->kick($reason !== "" ? "Banned by admin. Reason: " . $reason : "Banned by admin.");
+		}
 
 		Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
 
