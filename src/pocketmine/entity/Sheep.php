dae992597ba47afdbdf6021a42e8613982ca73b5
Erkam Kahraman <erkamkahriman@gmail.com>
2017-08-02 16:43:07 +0200

    Update Sheep.php

--- src/pocketmine/entity/Sheep.php
+++ src/pocketmine/entity/Sheep.php
@@ -31,102 +31,102 @@ use pocketmine\Player;
 
 class Sheep extends Animal implements Colorable {
 
-	const NETWORK_ID = 13;
+    const NETWORK_ID = 13;
 
-	const DATA_COLOR_INFO = 16;
+    const DATA_COLOR_INFO = 16;
 
-	public $width = 0.625;
-	public $length = 1.4375;
-	public $height = 1.8;
+    public $width = 0.625;
+    public $length = 1.4375;
+    public $height = 1.8;
 
-	/**
-	 * @return string
-	 */
-	public function getName() : string{
-		return "Sheep";
-	}
+    /**
+     * @return string
+     */
+    public function getName(): string{
+        return "Sheep";
+    }
 
-	/**
-	 * Sheep constructor.
-	 *
-	 * @param Level       $level
-	 * @param CompoundTag $nbt
-	 */
-	public function __construct(Level $level, CompoundTag $nbt){
-		if(!isset($nbt->Color)){
-			$nbt->Color = new ByteTag("Color", self::getRandomColor());
-		}
-		parent::__construct($level, $nbt);
+    /**
+     * Sheep constructor.
+     *
+     * @param Level $level
+     * @param CompoundTag $nbt
+     */
+    public function __construct(Level $level, CompoundTag $nbt){
+        if (!isset($nbt->Color)) {
+            $nbt->Color = new ByteTag("Color", self::getRandomColor());
+        }
+        parent::__construct($level, $nbt);
 
-		$this->setDataProperty(self::DATA_COLOR_INFO, self::DATA_TYPE_BYTE, $this->getColor());
-	}
+        $this->setDataProperty(self::DATA_COLOR_INFO, self::DATA_TYPE_BYTE, $this->getColor());
+    }
 
-	/**
-	 * @return int
-	 */
-	public static function getRandomColor() : int{
-		$rand = "";
-		$rand .= str_repeat(Wool::WHITE . " ", 20);
-		$rand .= str_repeat(Wool::ORANGE . " ", 5);
-		$rand .= str_repeat(Wool::MAGENTA . " ", 5);
-		$rand .= str_repeat(Wool::LIGHT_BLUE . " ", 5);
-		$rand .= str_repeat(Wool::YELLOW . " ", 5);
-		$rand .= str_repeat(Wool::GRAY . " ", 10);
-		$rand .= str_repeat(Wool::LIGHT_GRAY . " ", 10);
-		$rand .= str_repeat(Wool::CYAN . " ", 5);
-		$rand .= str_repeat(Wool::PURPLE . " ", 5);
-		$rand .= str_repeat(Wool::BLUE . " ", 5);
-		$rand .= str_repeat(Wool::BROWN . " ", 5);
-		$rand .= str_repeat(Wool::GREEN . " ", 5);
-		$rand .= str_repeat(Wool::RED . " ", 5);
-		$rand .= str_repeat(Wool::BLACK . " ", 10);
-		$arr = explode(" ", $rand);
-		return intval($arr[mt_rand(0, count($arr) - 1)]);
-	}
+    /**
+     * @return int
+     */
+    public static function getRandomColor(): int{
+        $rand = "";
+        $rand .= str_repeat(Wool::WHITE . " ", 20);
+        $rand .= str_repeat(Wool::ORANGE . " ", 5);
+        $rand .= str_repeat(Wool::MAGENTA . " ", 5);
+        $rand .= str_repeat(Wool::LIGHT_BLUE . " ", 5);
+        $rand .= str_repeat(Wool::YELLOW . " ", 5);
+        $rand .= str_repeat(Wool::GRAY . " ", 10);
+        $rand .= str_repeat(Wool::LIGHT_GRAY . " ", 10);
+        $rand .= str_repeat(Wool::CYAN . " ", 5);
+        $rand .= str_repeat(Wool::PURPLE . " ", 5);
+        $rand .= str_repeat(Wool::BLUE . " ", 5);
+        $rand .= str_repeat(Wool::BROWN . " ", 5);
+        $rand .= str_repeat(Wool::GREEN . " ", 5);
+        $rand .= str_repeat(Wool::RED . " ", 5);
+        $rand .= str_repeat(Wool::BLACK . " ", 10);
+        $arr = explode(" ", $rand);
+        return intval($arr[mt_rand(0, count($arr) - 1)]);
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getColor() : int{
-		return (int) $this->namedtag["Color"];
-	}
+    /**
+     * @return int
+     */
+    public function getColor(): int{
+        return (int)$this->namedtag["Color"];
+    }
 
-	/**
-	 * @param int $color
-	 */
-	public function setColor(int $color){
-		$this->namedtag->Color = new ByteTag("Color", $color);
-	}
+    /**
+     * @param int $color
+     */
+    public function setColor(int $color){
+        $this->namedtag->Color = new ByteTag("Color", $color);
+    }
 
-	/**
-	 * @param Player $player
-	 */
-	public function spawnTo(Player $player){
-		$pk = new AddEntityPacket();
-		$pk->eid = $this->getId();
-		$pk->type = Sheep::NETWORK_ID;
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->speedX = $this->motionX;
-		$pk->speedY = $this->motionY;
-		$pk->speedZ = $this->motionZ;
-		$pk->yaw = $this->yaw;
-		$pk->pitch = $this->pitch;
-		$pk->metadata = $this->dataProperties;
-		$player->dataPacket($pk);
+    /**
+     * @param Player $player
+     */
+    public function spawnTo(Player $player){
+        $pk = new AddEntityPacket();
+        $pk->eid = $this->getId();
+        $pk->type = Sheep::NETWORK_ID;
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->speedX = $this->motionX;
+        $pk->speedY = $this->motionY;
+        $pk->speedZ = $this->motionZ;
+        $pk->yaw = $this->yaw;
+        $pk->pitch = $this->pitch;
+        $pk->metadata = $this->dataProperties;
+        $player->dataPacket($pk);
 
-		parent::spawnTo($player);
-	}
+        parent::spawnTo($player);
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getDrops(){
-		$drops = [
-			ItemItem::get(ItemItem::WOOL, $this->getColor(), 1),
+    /**
+     * @return array
+     */
+    public function getDrops(){
+        $drops = [
+            ItemItem::get(ItemItem::WOOL, $this->getColor(), 1),
             ItemItem::get(ItemItem::RAW_MUTTON, 0, rand(1, 2))
-		];
-		return $drops;
-	}
+        ];
+        return $drops;
+    }
 }
