48918e7c9b1a9ad679568ca7eaab663aae3e9e66
Frago9876543210 <al.volf.av@gmail.com>
2017-08-03 19:50:07 +0300

    mcpe 1.1.5? WTF

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -32,8 +32,8 @@ interface Info {
 
 	const CURRENT_PROTOCOL = 113;
 	const ACCEPTED_PROTOCOLS = [110, 111, 112, 113];
-	const MINECRAFT_VERSION = ["v1.1.0", "v1.1.1", "v1.1.2", "v1.1.3", "v1.1.4"];
-	const MINECRAFT_VERSION_NETWORK = "1.1.4";
+	const MINECRAFT_VERSION = ["v1.1.0", "v1.1.1", "v1.1.2", "v1.1.3", "v1.1.4", "1.1.5"];
+	const MINECRAFT_VERSION_NETWORK = "1.1.5";
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
