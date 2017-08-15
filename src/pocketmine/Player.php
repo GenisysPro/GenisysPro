5c9d537d9da9ccf17d75bf526b0bfa9fb27a83fb
TDMidkwhatisxd <astermcpe@gmail.com>
2017-08-15 20:25:38 +0300

    Dont add IF on sendarmor
    if you add if , on send armorcontent, it will glitch.
    I tested it but please test before merging for security purpose.
    
    FIXED ARMOR GLITCH WERE IT SHOW YOU DONT HAVE IT, AND REAPEAR AFTER U GET HIT.

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1119,12 +1119,12 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 			$pk->z = $pos->z;
 			$this->dataPacket($pk);
 		}
-		if($this->inventory->sendContents($this) !== null){
+		
 			
 			$this->inventory->sendContents($this);
 			$this->inventory->sendArmorContents($this);
 			
-		}
+		
 	}
 
 	/**
