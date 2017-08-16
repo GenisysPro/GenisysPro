462c4dd0e96a7751edc9a0444e43671789500e34
Realm <Kavindujayasinghe2000@Gmail.com>
2017-08-16 16:52:03 +0530

    Get rid of AnimatePacket errors via revert

--- src/pocketmine/network/mcpe/protocol/AnimatePacket.php
+++ src/pocketmine/network/mcpe/protocol/AnimatePacket.php
@@ -30,14 +30,14 @@ class AnimatePacket extends DataPacket {
 
 	public $action;
 	public $eid;
-	public $float = 0.0; //TODO (Boat rowing time)
+	public $float;
 
 	/**
 	 *
 	 */
 	public function decode(){
-		$this->action = (int) $this->getUnsignedVarInt();
-		$this->eid = $this->getVarLong();
+		$this->action = $this->getVarInt();
+		$this->eid = $this->getEntityId();
 		if($this->float & 0x80){
 			$this->float = $this->getLFloat();
 		}
@@ -48,8 +48,8 @@ class AnimatePacket extends DataPacket {
 	 */
 	public function encode(){
 		$this->reset();
-		$this->putUnsignedVarInt($this->action);
-		$this->putVarLong($this->eid);
+		$this->putVarInt($this->action);
+		$this->putEntityId($this->eid);
 		if($this->float & 0x80){
 			$this->putLFloat($this->float);
 		}
