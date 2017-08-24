49fa5b1515937827e1d3c6cf7757fecb72bbc88a
Emre Yavuzyiğit <EmreTr1@users.noreply.github.com>
2017-08-24 14:32:01 +0300

    Added LevelSounds coming from client

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2377,6 +2377,9 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 		}
 
 		switch($packet::NETWORK_ID){
+			case ProtocolInfo::LEVEL_SOUND_EVENT_PACKET:
+			    $this->level->addChunkPacket($this->x >> 4, $this->z >> 4, $packet);
+			    break;
 			case ProtocolInfo::PLAYER_INPUT_PACKET:
 				break;
 			case ProtocolInfo::LOGIN_PACKET:
