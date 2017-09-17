4dbadda7e91d32a6171e7a7009812a7724385057
shoki <k.shoki@outlook.jp>
2017-09-17 18:20:08 +0900

    Update ProtocolInfo.php

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -32,7 +32,7 @@ interface ProtocolInfo {
 
 	const CURRENT_PROTOCOL = 113;
 	const ACCEPTED_PROTOCOLS = [110, 111, 112, 113];
-	const MINECRAFT_VERSION = ["v1.1.0", "v1.1.1", "v1.1.2", "v1.1.3", "v1.1.4", "v1.1.5", "v1.1.6", "v1.1.7"];
+	const MINECRAFT_VERSION = ["v1.1.0", "v1.1.1", "v1.1.2", "v1.1.3", "v1.1.4", "v1.1.5", "v1.1.7"];
 	const MINECRAFT_VERSION_NETWORK = "1.1.7";
 
 	const LOGIN_PACKET = 0x01;
